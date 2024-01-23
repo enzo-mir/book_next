61b0108e400bf80544140e2a6ca3eb95d44af276
Lagacy_code <miraglioenzo93@gmail.com>
2024-01-23 16:47:24 +0100

    style : update card grid, box shadow bottom card

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 import Nav from "./components/nav";
-import style from "./style/page.module.css";
 import Projects from "./components/projects";
 
 export default function Home() {
   return (
-    <main className={style.main}>
+    <main>
       <Nav />
       <Projects />
     </main>
