a454525296a621765538df4b04dcfac36fe384f2
Lagacy_code <miraglioenzo93@gmail.com>
2024-03-15 20:11:56 +0100

    feat : add new project => garden party

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -4,6 +4,7 @@ import style from "../style/book/projects.module.css";
 import { filter_store } from "../data/filter_store";
 import projects from "../data/data.json";
 import { projectsType } from "../types/projectsType";
+import FlipMove from "react-flip-move";
 const Projects = () => {
   const filter = filter_store((state) => state.filter);
 
@@ -46,66 +47,45 @@ const Projects = () => {
       return filterByDate(filterBySearch(filter.search));
     }
   }
-  const calculateColumns = () => {
-    const screenWidth = window.innerWidth;
-    if (screenWidth > 1200) {
-      return 5;
-    } else if (screenWidth > 900) {
-      return 4;
-    } else if (screenWidth > 600) {
-      return 3;
-    } else {
-      return 2;
-    }
-  };
-
-  const columnsData = Array.from({ length: calculateColumns() }, (_, i) => {
-    const itemsPerColumn = Math.ceil(filteredData().length / calculateColumns());
-    return filteredData().slice(i * itemsPerColumn, (i + 1) * itemsPerColumn);
-  });
 
   return (
-    <div className={style.projectsContainer}>
+    <FlipMove className={style.projectsContainer}>
       {filteredData().length ? (
-        columnsData.map((col, i) => {
+        filteredData().map((project, i) => {
           return (
-            <ul key={i} className={style.ul_card}>
-              {col.map((project, index) => {
-                return (
-                  <li key={index}>
-                    <article className={style.card}>
-                      <div className={style.headerCard}>
-                        <h1>{project.title}</h1>
-                        <img alt={project.title} src={project.img_url} property="high" width="300" height="150" />
-                      </div>
-                      <div className={style.bottomCard}>
-                        <sub>{project.date}</sub>
-                        <p>{project.description}</p>
-                        <ul className={style.tags}>
-                          {project.tag.map((tag, index) => {
-                            return <li key={index}>{tag}</li>;
-                          })}
-                        </ul>
-                        <div className={style.link}>
-                          <a href={project.git_url} target="_blank">
-                            #github
-                          </a>
-                          <a href={project.webiste_url} target="_blank">
-                            Visit webiste
-                          </a>
-                        </div>
-                      </div>
-                    </article>
-                  </li>
-                );
-              })}
-            </ul>
+            <article className={style.card} key={project.id}>
+              <div className={style.headerCard}>
+                <h1>{project.title}</h1>
+                <img alt={project.title} src={project.img_url} property="high" width="300" height="150" />
+              </div>
+              <div className={style.bottomCard}>
+                <sub>{project.date}</sub>
+                <p>{project.description}</p>
+                <ul className={style.tags}>
+                  {project.tag.map((tag, index) => {
+                    return <li key={index}>{tag}</li>;
+                  })}
+                </ul>
+                <div className={style.link}>
+                  {project.git_url ? (
+                    <a href={project.git_url} target="_blank">
+                      #github
+                    </a>
+                  ) : (
+                    <p>Private</p>
+                  )}
+                  <a href={project.webiste_url} target="_blank">
+                    Visit webiste
+                  </a>
+                </div>
+              </div>
+            </article>
           );
         })
       ) : (
         <p>No project corresponding to filter</p>
       )}
-    </div>
+    </FlipMove>
   );
 };
 
