d90d319be7ebbef816ee5883868ff0b23da85932
Lagacy_code <miraglioenzo93@gmail.com>
2024-04-15 17:37:31 +0200

    style : responsive design => homepage, portfolio grid

--- app/components/project_card.tsx
+++ app/components/project_card.tsx
@@ -5,7 +5,9 @@ import Link from "next/link";
 import { handleDragStart } from "../services/distorsion";
 import data from "../provider/data.json";
 import { motion } from "framer-motion";
+import { Work_Sans } from "next/font/google";
 
+const workSans = Work_Sans({ subsets: ["latin"], weight: "600" });
 const ProjectCard = ({ containerRef, filter }: { containerRef: React.RefObject<ElementRef<"section">>; filter: typeof data }) => {
   const sliderRef = useRef<HTMLDivElement>(null);
   const [position, setPosition] = useState(0);
@@ -23,7 +25,7 @@ const ProjectCard = ({ containerRef, filter }: { containerRef: React.RefObject<E
         return (
           <motion.article key={project.id} className={styles.article} initial={{ y: -50, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
             <div>
-              <h2>{project.title}</h2>
+              <h2 className={workSans.className}>{project.title}</h2>
               <Link href={`/portfolio/${project.id}`}>Leanr more +</Link>
             </div>
             <Image fill objectFit="cover" alt="projects" src={project.img_url} />
