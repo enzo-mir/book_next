8c5ed1cb9a7c68d6b5fde872ac0bd63ff5fc897a
Lagacy_code <miraglioenzo93@gmail.com>
2024-05-24 12:26:19 +0200

    fix : portfolio component repeating

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,14 +1,31 @@
 "use client";
-import React, { ElementRef, useRef } from "react";
+import React, { ElementRef, RefObject, useRef } from "react";
 import styles from "#styles/portfolio.module.css";
 import ProjectCard from "#components/project_card";
 import { filterByAdonis, filterByApi, filterByHtml } from "../services/filter_projects";
 import { motion } from "framer-motion";
 import { Work_Sans } from "next/font/google";
+import data from "../provider/data.json";
 
 const workSans = Work_Sans({ subsets: ["latin"], weight: "600" });
-const Page = () => {
+
+const FilteredElement = ({ filter, text }: { filter: typeof data; text: string }) => {
   const containerRef = useRef<ElementRef<"section">>(null);
+
+  return (
+    <section className={styles.section} ref={containerRef}>
+      <div>
+        <motion.h1 initial={{ y: -50, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
+          {text}
+        </motion.h1>
+        <motion.hr initial={{ width: "0%" }} animate={{ width: "100%", transition: { duration: 0.5 } }} />
+      </div>
+      <ProjectCard containerRef={containerRef} filter={filter} />
+    </section>
+  );
+};
+
+const Page = () => {
   return (
     <main className={styles.main}>
       <section>
@@ -19,30 +36,10 @@ const Page = () => {
           Drag it !
         </motion.p>
       </section>
-      <section className={styles.section} ref={containerRef}>
-        <div>
-          <motion.h1 initial={{ y: -50, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
-            html, css, js Vanillia
-          </motion.h1>
-          <motion.hr initial={{ width: "0%" }} animate={{ width: "100%", transition: { duration: 0.5 } }} />
-        </div>
-        <ProjectCard containerRef={containerRef} filter={filterByHtml} />
-      </section>
-      <section className={styles.section} ref={containerRef}>
-        <div>
-          <h1>api, json</h1>
-          <hr />
-        </div>
-        <ProjectCard containerRef={containerRef} filter={filterByApi} />
-      </section>
 
-      <section className={styles.section} ref={containerRef}>
-        <div>
-          <h1>adonisjs, react, typescript</h1>
-          <hr />
-        </div>
-        <ProjectCard containerRef={containerRef} filter={filterByAdonis} />
-      </section>
+      <FilteredElement filter={filterByHtml} text="html, css, js Vanillia" />
+      <FilteredElement filter={filterByApi} text="api, json" />
+      <FilteredElement filter={filterByAdonis} text="adonisjs, react, typescript" />
     </main>
   );
 };
